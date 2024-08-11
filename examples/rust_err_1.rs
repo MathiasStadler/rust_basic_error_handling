b2bca361f1e25c48c74594f37602ee65604f8bd3
Mathias <email@example.com>
2024-08-11 15:02:17 +0000

    finish for today

--- examples/rust_err_1.rs
+++ examples/rust_err_1.rs
@@ -1,11 +1,24 @@
 // FROM HERE
 // https://stevedonovan.github.io/rust-gentle-intro/6-error-handling.html
 
+// box-error.rs
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
 
-
-
+fn run(file: &str) -> Result<i32, Box<dyn Error>> {
+    let mut file = File::open(file)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents.trim().parse()?)
+}
 
 fn main() {
+    let result_run = run("not_availble.txt");
+    let file_name = match result_run {
+        Ok(file_name) => file_name,
+        Err(e) => Err(e),
+    };
 }
 
 // cargo run --example rust_err_1
